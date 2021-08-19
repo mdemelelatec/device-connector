0d031095c41abe94df835435d1101c6637b19c28
AchimVogel <Achim.Vogel@freenet.de>
2021-08-19 16:25:05 +0200

    Delete server.js

--- server.js
+++
@@ -1,6 +0,0 @@
-const cds = require ('@sap/cds')
-module.exports = cds.server
-console.log("Starting");
-
-  const cds_swagger = require ('cds-swagger-ui-express')
-  cds.on ('bootstrap', app => app.use (cds_swagger()) )
